7f60a723ff81f83a5bd1b45abc1bf439d371364c
connormcguire02 <34112443+connormcguire02@users.noreply.github.com>
2022-09-29 23:55:09 -0500

    "Update"

---
+++ howdy_whoop.py
@@ -0,0 +1,24 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Connor McGuire
+# Section:      563
+# Assignment:   Lab 5.4
+# Date:         29 9 2022
+
+# prompting the user to enter two numbers
+num1 = int(input("Enter an integer: "))
+num2 = int(input("Enter another integer: "))
+
+# creating a loop that will go through all 100 numbers and check for divisibility
+i = 1
+while i <= 100:
+    print(i)
+    if i%num1 == 0 and i%num2 == 0:
+        print("Howdy Whoop")
+    elif i%num1 == 0:
+        print("Howdy")
+    elif i%num2 == 0:
+        print("Whoop")
+    i += 1
