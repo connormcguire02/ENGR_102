74a78ede6b3bdf8967795971f77e00a73b7981e0
connormcguire02 <34112443+connormcguire02@users.noreply.github.com>
2022-10-17 08:40:22 -0500

    "Update"

---
+++ ascii_clock_cm.py
@@ -0,0 +1,5 @@
+clock = ["1","2",":","5","6"]
+
+n = 0
+while n < len(clock):
+    pass
--- kaprekars_constant.py
+++ kaprekars_constant.py
@@ -7,12 +7,37 @@
 # Assignment:   Lab 5.4
 # Date:         7 10 2022
 
+def bubbleSort(a, bool):
+    if isinstance(a, str):
+        b = ""
+        a_list = sorted(a, reverse=bool)
+        n = 0
+        while n < len(a_list):
+            b += a_list[n]
+            n += 1
+        return b
+
 #prompting the user to enter a 4 digit number
 string_number = input("Enter a four-digit integer: ")
 number = int(string_number)
+n = 0
+while n < 3:
+    # the loop needs a reinitialization of the string number
+    string_number = str(number)
 
-# initializing the reversed numbers
-string_backwards = ""
-number_backwards = 0
+    # initializing the reversed numbers
+    string_sorted = ""
+    number_backwards = 0
 
-string_backwards = string_number[len(string_number):0]
+    # using my sort method to sort the string
+    string_descending = bubbleSort(string_number, True)
+    string_ascending = bubbleSort(string_number, False)
+    number_ascending = int(string_ascending)
+    number_descending = int(string_descending)
+    # print("String ascending:",string_ascending)
+    # print("String descending:",string_descending)
+
+    # finding which number is bigger and subtracting the bigger from the smaller
+    number = abs(number_ascending-number_descending)
+    # print(number)
+    n += 1
