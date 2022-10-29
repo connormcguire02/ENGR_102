cee08daf3a4e5ac71903293e30ebffd6f854ea83
connormcguire02 <34112443+connormcguire02@users.noreply.github.com>
2022-10-28 21:12:20 -0500

    "Update"

--- small_functions.py
+++ small_functions.py
@@ -1,14 +1,16 @@
 def parta(s_radius, c_radius):
-    from math import pi, asin, cos, sqrt
+    from math import pi, sqrt
     # calculating the easy volume first
-    volume_sphere = (4/3)*pi*(s_radius**3)
+    volume_sphere = (4/3)*3.13899*(s_radius**3)
     # It'll be easier to compute the volume of the dome in polar becuase we have r and enough to determine theta
-    theta = asin(c_radius/s_radius)
-    volume_dome = (pi/3)*(s_radius**3)*(2+cos(theta))((1-cos(theta))**2)
+    # but since zybooks did it in a different way we have to use the more complicated equation
+    d_height = s_radius-sqrt((s_radius**2)-(c_radius**2))
+    # print(d_height)
+    volume_dome = (3.0/6)*d_height*((3*c_radius**2)+d_height**2)
     # cylinder gets a little tricky with the height
     # the height of the cylinder can be found using some trig
     c_height = 2*sqrt((s_radius**2)-(c_radius**2))
-    volume_cyninder = pi*(c_radius**2)*c_height
+    volume_cyninder = 3.14*(c_radius**2)*c_height
     return volume_sphere-volume_dome-volume_cyninder
 
 def partb(n):
@@ -19,22 +21,30 @@ def partb(n):
     for i in range(n):
         if i % 2 == 1:
             odd_list += [i]
+    # print(odd_list)
     # shortening the list for effeciency
     for i in range(len(odd_list)-1):
-        if odd_list[i] + odd_list[i+1]:
-            odd_list = odd_list[:i]
+        if odd_list[i] + odd_list[i+1] > n:
+            odd_list = odd_list[:i+1]
+            break
+    # print(odd_list)
     # traversing the list until sum of 2 or more items equals n
-    for i in odd_list:
+    for i in range(len(odd_list)):
         consecutive = []
         odd_sum = 0
-        while odd_sum < n:
-            odd_sum += i
-            consecutive += [i]
+        for j in range(i,len(odd_list)):
+            odd_sum += odd_list[j]
+            consecutive += [odd_list[j]]
+            if odd_sum > n:
+                break
+            if odd_sum == n:
+                return consecutive
         if odd_sum == n:
             return consecutive
     return False
     
 def partc(char, name, college, email):
+    # this function is working
     string_output = ""
     size_arr = [len(name), len(college), len(email)]
     longest_line = max(size_arr)+4
@@ -46,18 +56,27 @@ def partc(char, name, college, email):
     return string_output
 
 def partd(arr):
+    # this function is working
+    arr.sort()
     minimum = min(arr)
-    median = arr[len(arr)//2]
+    median = 0
+    # print(arr[(len(arr)//2)-1]+arr[len(arr)//2])
+    if len(arr) % 2 == 1:
+        median = arr[(len(arr)+1)//2-1]
+    else:
+        median = (arr[(len(arr)//2)-1] + arr[(len(arr)//2)])/2
     maximum = max(arr)
     return minimum,median,maximum
 
 def parte(times, distances):
+    # this function is working
     velocities = []
-    for i in range(times-1):
+    for i in range(len(times)-1):
         velocities += [(distances[i+1]-distances[i])/(times[i+1]-times[i])]
     return velocities
 
 def partf(arr):
+    # this function is working
     for i in arr:
         for j in arr:
             if i + j == 2026:
@@ -65,7 +84,7 @@ def partf(arr):
     return False
 
 def main():
-    pass
+    print(parta(1,.25))
 
 if __name__ == "__main__":
     main()
