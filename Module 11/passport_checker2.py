2d29fd1a3d67c3d2327c67c94efbeefd26e429ae
connormcguire02 <34112443+connormcguire02@users.noreply.github.com>
2022-11-10 13:08:05 -0600

    "update"

--- Module 11/passport_checker2.py
+++ Module 11/passport_checker2.py
@@ -10,39 +10,28 @@
 # Assignment:   Lab 11.10
 # Date:         11/10/22
 
-'''
-• byr – Birth year – four digits, between 1920 and 2005, inclusive
-• iyr – Issue year – four digits, between 2012 and 2022, inclusive
-• eyr – Expiration year – four digits, between 2022 and 2032, inclusive
-• hgt – Height – a number followed by either cm or in
-o If cm, the number must be between 150 and 193, inclusive
-o If in, the number must be between 59 and 76, inclusive
-• hcl – Hair color – not required
-• ecl – Eye color – exactly one of the following: amb, blu, brn, gry, grn, hzl, oth
-• pid – Passport ID – a nine-digit number, including leading zeroes
-• cid – Country ID – a three-digit number, NOT including leading zeroes
-
-writes the valid passport scans to a new file named valid_passports2.txt.
-'''
-
 filename = input("Enter the name of the file: ")
 
 valid = 0
 
-validpass = open('valid_passports2.txt', 'w') # create the valid passports file
+validpass = open('valid_passports.txt', 'w') # create the valid passports file
 
 valid_list = []
 
-# think we could use same baseline as first code
-# and just add all the other checking things to it
-
-with open(filename, "r+") as filehandler:
-    read = filehandler.read()
-    passlist = read.split('\n\n')
+with open(filename, "r+") as filehandler: # open the file
+    read = filehandler.read() # make the file a long string
+    passlist = read.split('\n\n') # make the file list based on passport
     for passport in passlist:
-        personal_info = passport.split("\n")
-    print(personal_info)
-    valid_list = valid_list.split(" ", '\n') 
-print(f'There are {valid} valid passports')
+        if 'byr' not in passport or 'iyr' not in passport or 'eyr' not in passport or 'hgt' not in passport or 'ecl' not in passport or 'pid' not in passport or 'cid' not in passport: # if not valid, go to next passport
+            continue
+        else: # if valid, add number to valid variable
+            valid_list.append(passport)
+            valid += 1
+    for i in range(len(valid_list)):
+        valid_list[i] = valid_list[i].split('\n')
+        valid_list[i] = ' '.join(valid_list[i])
+        valid_list[i] = valid_list[i].split()
+    print(valid_list)
+print(f'There are {valid} valid passports') # print the number of valid passports
 
-validpass.close()
+validpass.close() # close the valid passport file
