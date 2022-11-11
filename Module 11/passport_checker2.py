db3fd454fbb054927368e676c0ca8c520c1049ef
connormcguire02 <34112443+connormcguire02@users.noreply.github.com>
2022-11-11 15:15:47 -0600

    "Update"

--- Module 11/passport_checker2.py
+++ Module 11/passport_checker2.py
@@ -20,71 +20,70 @@ valid_list = []
 
 valid_ones = []
 
-with open(foldername+"\\"+filename, "r+") as filehandler: # open the file
-    read = filehandler.read() # make the file a long string
-    passlist = read.split('\n\n') # make the file list based on passport
+with open(filename, "r+") as filehandler: # open the file
+    read = filehandler.read()  # make the file a long string
+    passlist = read.split('\n\n')  # make the file list based on passport
     for passport in passlist:
-        if 'byr' not in passport or 'iyr' not in passport or 'eyr' not in passport or 'hgt' not in passport or 'ecl' not in passport or 'pid' not in passport or 'cid' not in passport: # if not valid, go to next passport
+        if 'byr' not in passport or 'iyr' not in passport or 'eyr' not in passport or 'hgt' not in passport or 'ecl' not in passport or 'pid' not in passport or 'cid' not in passport:  # if not valid, go to next passport
             continue
-        else: # if valid, add number to valid variable
+        else:  # if valid, add number to valid variable
             valid_ones.append(passport)
-            valid += 1
-    valid_list = valid_ones
+    valid_list = valid_ones[:]
     for i in range(len(valid_list)):
         valid_list[i] = valid_list[i].split('\n')
         valid_list[i] = ' '.join(valid_list[i])
         valid_list[i] = valid_list[i].split()
         for j in range(len(valid_list[i])):
             valid_list[i][j] = valid_list[i][j].split(':')
-    # print(valid_list)
     for i in range(len(valid_list)):
         for j in range(len(valid_list[i])):
+            yes = True
             if valid_list[i][j][0] == 'byr':
-                if 1920 <= int(valid_list[i][j][1]) <= 2005:
-                    continue
-                else:
+                if 1920 > int(valid_list[i][j][1]) or 2005 < int(valid_list[i][j][1]):
+                    yes = False
                     break
             elif valid_list[i][j][0] == 'iyr':
-                if 2011 < int(valid_list[i][j][1]) < 2023:
-                    continue
-                else:
+                if 2011 >= int(valid_list[i][j][1]) or 2023 <= int(valid_list[i][j][1]):
+                    yes = False
                     break
             elif valid_list[i][j][0] == 'eyr':
-                if 2022 <= int(valid_list[i][j][1]) <= 2032:
-                    continue
+                if 2022 > int(valid_list[i][j][1]) or 2032 <= int(valid_list[i][j][1]):
+                    yes = False
+                    break
             elif valid_list[i][j][0] == 'hgt':
-                if 'cm' in valid_list[i][j][1] and len(valid_list[i][j][1]) > 4:
-                    if 150 <= int(valid_list[i][j][1][0:3]) <= 193:
-                        continue
-                    else:
-                        break
+                if 'cm' in valid_list[i][j][1]:
+                    centi = valid_list[i][j][1].split('c')
+                    if 150 >= int(centi[0]) or 193 <= int(centi[0]):
+                        # print(centi)
+                        yes = False
+                        break # for some reason the code is still adding 66 cm passport to the valid_passports2 file !!! i think height is the problem one, just kidding. it is also not catching other things and i dont get why
+                    break
                 elif 'in' in valid_list[i][j][1]:
-                    if 59 <= int(valid_list[i][j][1][0:2]) <= 76:
-                        continue
-                    else:
+                    inch = valid_list[i][j][1].split('i')
+                    if 59 >= int(inch[0]) or 76 <= int(inch[0]):
+                        yes = False
                         break
-                else:
                     break
             elif valid_list[i][j][0] == 'ecl':
-                if valid_list[i][j][1] == 'amb' or valid_list[i][j][1] == 'brn' or valid_list[i][j][1] == 'blu' or valid_list[i][j][1] == 'gry' or valid_list[i][j][1] == 'grn' or valid_list[i][j][1] == 'hzl' or valid_list[i][j][1] == 'oth':
-                    continue
-                else: 
+                if valid_list[i][j][1] != 'amb' and valid_list[i][j][1] != 'brn' and valid_list[i][j][1] != 'blu' and valid_list[i][j][1] != 'gry' and valid_list[i][j][1] != 'grn' and valid_list[i][j][1] != 'hzl' and valid_list[i][j][1] != 'oth':
+                    yes = False
                     break
             elif valid_list[i][j][0] == 'pid':
-                if len(valid_list[i][j][1]) == 9:
-                    continue
-                else:
+                if len(valid_list[i][j][1]) != 9:
+                    yes = False
                     break
             elif valid_list[i][j][0] == 'cid':
-                if int(valid_list[i][j][1])//100 >= 1:
-                    continue
-                else:
+                num = int(valid_list[i][j][1])
+                if len(str(num)) != 3:
+                    yes = False
                     break
-            else:
-                continue
-        # if valid_list[i][j] == valid_ones[j]:
-            # validpass.write(valid_ones[j]='\n')
-        
-print(f'There are {valid} valid passports') # print the number of valid passports
+            if j == (len(valid_list[i][j]) - 1) and yes == True:
+                validpass.write(valid_ones[i] + '\n\n')
+                valid += 1
 
-validpass.close() # close the valid passport file
+#print(valid_ones)
+#print(valid_list)
+
+print(f'There are {valid} valid passports')  # print the number of valid passports
+
+validpass.close()  # close the valid passport file
