5553af1db2bdf67df203c765ec04d5dcbbed2c79
connormcguire02 <34112443+connormcguire02@users.noreply.github.com>
2022-11-13 16:49:29 -0600

    "Update"

--- Module 11/count_coins.py
+++ Module 11/count_coins.py
@@ -10,5 +10,20 @@
 with open("game.txt", "r") as filehandler:
      entire_text = filehandler.read()
      instructions = entire_text.split("\n")
-     for i in instructions:
-        i = i.split()
+     i = 0
+     coin_accum = 0
+     while i < len(instructions):
+        instructions[i] = instructions[i].split()
+        if instructions[i][0] == 'jump':
+            if '+' in instructions[i][1]:
+                i += int(instructions[i][1][1:])
+            else:
+                i -= int(instructions[i][1][1:])
+        if instructions[i][0] == 'none':
+            i += 1
+        if instructions[i][0] == 'coin':
+            if '+' in instructions[i][1]:
+                coin_accum += int(instructions[i][1][1:])
+            else:
+                coin_accum -= int(instructions[i][1][1:])
+
