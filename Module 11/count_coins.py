282223e72fafe769e867a694db577b0fb50ecc23
connormcguire02 <34112443+connormcguire02@users.noreply.github.com>
2022-11-15 09:13:21 -0600

    "Update"

--- Module 11/count_coins.py
+++ Module 11/count_coins.py
@@ -7,23 +7,33 @@
 # Assignment:   Lab 9.16
 # Date:         28 10 2022
 
+filewriter = open("coins.txt", "w")
+
 with open("game.txt", "r") as filehandler:
-     entire_text = filehandler.read()
-     instructions = entire_text.split("\n")
-     i = 0
-     coin_accum = 0
-     while i < len(instructions):
+    entire_text = filehandler.read()
+    instructions = entire_text.split("\n")
+    i = 0
+    coin_accum = 0
+    while i < len(instructions):
         instructions[i] = instructions[i].split()
+        print(instructions[i])
         if instructions[i][0] == 'jump':
             if '+' in instructions[i][1]:
                 i += int(instructions[i][1][1:])
+                filewriter.write(instructions[i][1][0]+"\n")
             else:
                 i -= int(instructions[i][1][1:])
+                filewriter.writelines(instructions[i][1][1:]+"\n")
+            continue
         if instructions[i][0] == 'none':
             i += 1
+            continue
         if instructions[i][0] == 'coin':
             if '+' in instructions[i][1]:
                 coin_accum += int(instructions[i][1][1:])
+                i += 1
             else:
                 coin_accum -= int(instructions[i][1][1:])
-
+                i += 1
+            continue
+    print(f"Total coins collected: {coin_accum}")
